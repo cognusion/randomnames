2a6e7ef59333209c7e4201259db57f152f821e9d
M <m@cognusion.com>
2017-07-19 22:37:37 -0400

    Adds more fun combo functions

--- common.go
+++ common.go
@@ -19,3 +19,23 @@ func RandomAdjectiveAnimal() string {
 func SafeRandomAdjectiveAnimal() string {
 	return fmt.Sprintf("%s %s", SafeRandomAdjective(), SafeRandomAnimal())
 }
+
+// RandomNameAnimal returns a pseudo-random name and animal from the lists
+func RandomNameAnimal() string {
+	return fmt.Sprintf("%s %s", RandomName(), RandomAnimal())
+}
+
+// SafeRandomNameAnimal returns a pseudo-random name and animal from the lists goro-safely
+func SafeRandomNameAnimal() string {
+	return fmt.Sprintf("%s %s", SafeRandomName(), SafeRandomAnimal())
+}
+
+// RandomAdjectiveName returns a pseudo-random adjective and name from the lists
+func RandomAdjectiveName() string {
+	return fmt.Sprintf("%s %s", RandomAdjective(), RandomName())
+}
+
+// SafeRandomAdjectiveName returns a pseudo-random adjective and animal from the lists goro-safely
+func SafeRandomAdjectiveName() string {
+	return fmt.Sprintf("%s %s", SafeRandomAdjective(), SafeRandomName())
+}
